b73b9c74fb484021324934765b6bc2bd827117a5
shakhovtsov <shahovsov8v@gmail.com>
2023-01-12 20:28:55 +0200

    arrange slider

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const btnComboRight = document.querySelector('.combo__btn');
+
+let comboSliderBox = document.querySelectorAll('.combo__slider-box');
+
+let i = 0;
+btnComboRight.onclick = function () {
+    comboSliderBox[i].style.display = 'none';
+    i++;
+    if (i >= comboSliderBox.length) {
+        i = 0;
+    }
+    comboSliderBox[i].style.display = 'flex';
+}
+
+
+const btnChooseRight = document.querySelector('.choose__btn');
+let chooseSliderBox = document.querySelectorAll('.choose__slider-box');
+btnChooseRight.onclick = function () {
+    chooseSliderBox[i].style.display = 'none';
+    i++;
+    if (i >= comboSliderBox.length) {
+        i = 0;
+    }
+    chooseSliderBox[i].style.display = 'flex';
+}
